6883ced539af4fceb96a502f9b5f45da682fad52
shamad <fuzzriff@gmail.com>
2024-01-31 21:04:21 +0300

    done

--- 3-calc/index.js
+++ 3-calc/index.js
@@ -5,4 +5,6 @@ const [, , number1, number2, operation] = process.argv;
 
 if (operation === "add") {
     console.log(adder(number1, number2))
+} else {
+    console.log("To perform add input: node index.js num1 num2 add")
 }
